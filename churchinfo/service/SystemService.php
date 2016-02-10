1ee4a261941021162c52521e76ece93b0195a2c1
Daniel Schwen <daniel.schwen@inl.gov>
2016-02-10 15:55:44 -0700

    Use PHP builtins rather than exec in SystemService (#308)

--- churchinfo/service/SystemService.php
+++ churchinfo/service/SystemService.php
@@ -6,19 +6,22 @@ class SystemService {
     {
         $query = '';
         $restoreQueries = file($fileName, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach ($restoreQueries as $line) {
-            if ($line != '' && strpos($line, '--') === false) {
+        foreach ($restoreQueries as $line)
+        {
+            if ($line != '' && strpos($line, '--') === false)
+            {
                 $query .= $line;
-                if (substr($query, -1) == ';') {
+                if (substr($query, -1) == ';')
+                {
                     $person = RunQuery($query);
                     $query = '';
                 }
             }
         }
-        
     }
 
-    function restoreDatabaseFromBackup(){
+    function restoreDatabaseFromBackup()
+    {
         $restoreResult = new StdClass();
         global $sUSER, $sPASSWORD, $sDATABASE, $cnInfoCentral,$sGZIPname;
         $file = $_FILES['restoreFile'];
@@ -30,25 +33,21 @@ class SystemService {
         $restoreResult->imagesRoot = "Images";
         $restoreResult->headers = array();
         // Delete any old backup files
-        exec("rm -rf  $restoreResult->backupRoot");
-        exec("mkdir  $restoreResult->backupRoot");
-        if ($restoreResult->type ==  "gz")
-        {   
-            if ($restoreResult->type2 ==  "tar")
+        unlink($restoreResult->backupRoot);
+        mkdir($restoreResult->backupRoot);
+        if ($restoreResult->type == "gz")
+        {
+            if ($restoreResult->type2 == "tar")
             {
-                exec ("mkdir $restoreResult->backupRoot");
                 $restoreResult->uncompressCommand = "tar -zxvf ".$file['tmp_name']." --directory $restoreResult->backupRoot";
                 exec($restoreResult->uncompressCommand, $rs1, $returnStatus);
                 $restoreResult->SQLfile = "$restoreResult->backupRoot/ChurchCRM-Database.sql";
                 $this->playbackSQLtoDatabase($restoreResult->SQLfile);
-                exec("rm -rf $restoreResult->root/Images/*");
-                exec("mv -f $restoreResult->backupRoot/Images/* $restoreResult->root/Images");
-                
+                rename($restoreResult->backupRoot/Images, $restoreResult->root/Images);
             }
-            else if ($restoreResult->type2 ==  "sql")
+            else if ($restoreResult->type2 == "sql")
             {
-                exec ("mkdir $restoreResult->backupRoot");
-                exec ("mv  ".$file['tmp_name']." ".$restoreResult->backupRoot."/".$file['name']);
+                rename($file['tmp_name'], $restoreResult->backupRoot."/".$file['name']);
                 $restoreResult->uncompressCommand = "sGZIPname -d $restoreResult->backupRoot/".$file['name'];
                 exec($restoreResult->uncompressCommand, $rs1, $returnStatus);;
                 $restoreResult->SQLfile = $restoreResult->backupRoot."/".substr($file['name'],0,strlen($file['name'])-3);
@@ -59,23 +58,25 @@ class SystemService {
         {
             $this->playbackSQLtoDatabase($file['tmp_name']);
         }
-        exec ("rm -rf $restoreResult->backupRoot"); 
+        unlink($restoreResult->backupRoot);
         $this->rebuildMenus();
         $restoreResult->UpgradeStatus = $this->checkDatabaseVersion();
        return $restoreResult;
-        
+
     }
-    function getDatabaseBackup($params) {
-        global $sUSER, $sPASSWORD, $sDATABASE, $sSERVERNAME, $sGZIPname, $sZIPname, $sPGPname; 
-        
+
+    function getDatabaseBackup($params)
+    {
+        global $sUSER, $sPASSWORD, $sDATABASE, $sSERVERNAME, $sGZIPname, $sZIPname, $sPGPname;
+
         $backup = new StdClass();
         $backup->root = dirname(dirname(__FILE__));
         $backup->backupRoot="$backup->root/tmp_attach/ChurchCRMBackups";
         $backup->imagesRoot = "Images";
         $backup->headers = array();
         // Delete any old backup files
-        exec("rm -rf  $backup->backupRoot");
-        exec("mkdir  $backup->backupRoot");
+        unlink($backup->backupRoot);
+        mkdir($backup->backupRoot);
         // Check to see whether this installation has gzip, zip, and gpg
         if (isset($sGZIPname)) $hasGZIP = true;
         if (isset($sZIPname)) $hasZIP = true;
@@ -83,72 +84,73 @@ class SystemService {
 
         $backup->params = $params;
         $bNoErrors = true;
-        
-        $backup->saveTo = "$backup->backupRoot/ChurchCRM-".date("Ymd-Gis");
-		$backup->SQLFile = "$backup->backupRoot/ChurchCRM-Database.sql";
-       
-		$backupCommand = "mysqldump -u $sUSER --password=$sPASSWORD --host=$sSERVERNAME $sDATABASE > $backup->SQLFile";
-		exec($backupCommand, $returnString, $returnStatus);
 
-		switch ($params->iArchiveType)
-		{
-			case 0: # The user wants a gzip'd SQL file. 
+        $backup->saveTo = "$backup->backupRoot/ChurchCRM-".date("Ymd-Gis");
+        $backup->SQLFile = "$backup->backupRoot/ChurchCRM-Database.sql";
+
+        $backupCommand = "mysqldump -u $sUSER --password=$sPASSWORD --host=$sSERVERNAME $sDATABASE > $backup->SQLFile";
+        exec($backupCommand, $returnString, $returnStatus);
+
+        switch ($params->iArchiveType)
+        {
+            case 0: # The user wants a gzip'd SQL file.
                 $backup->saveTo.=".sql";
-                exec("mv $backup->SQLFile  $backup->saveTo");
+                rename($backup->SQLFile, $backup->saveTo);
                 $backup->compressCommand =  "$sGZIPname $backup->saveTo";
-				$backup->saveTo .= ".gz";
-				exec($backup->compressCommand, $returnString, $returnStatus);
+                $backup->saveTo .= ".gz";
+                exec($backup->compressCommand, $returnString, $returnStatus);
                 $backup->archiveResult = $returnString;
-				break;
-			case 1: #The user wants a .zip file
-				$backup->saveTo.=".zip";
+                break;
+            case 1: #The user wants a .zip file
+                $backup->saveTo.=".zip";
                 $backup->compressCommand = "$sZIPname -r -y -q -9 $backup->saveTo $backup->backupRoot";
-				exec($backup->compressCommand, $returnString, $returnStatus);
+                exec($backup->compressCommand, $returnString, $returnStatus);
                 $backup->archiveResult = $returnString;
-				break;
+                break;
             case 2: #The user wants a plain ol' SQL file
                 $backup->saveTo.=".sql";
-                exec("mv $backup->SQLFile  $backup->saveTo");
+                rename($backup->SQLFile, $backup->saveTo);
                 break;
             case 3: #the user wants a .tar.gz file
                 $backup->saveTo.=".tar.gz";
                 $backup->compressCommand ="tar -zcvf $backup->saveTo -C $backup->backupRoot ChurchCRM-Database.sql -C $backup->root $backup->imagesRoot";
-				exec($backup->compressCommand, $returnString, $returnStatus);
+                exec($backup->compressCommand, $returnString, $returnStatus);
                 $backup->archiveResult = $returnString;
-				break;
-             
-		}
+                break;
 
-		if ($params->bEncryptBackup)  #the user has selected an encrypted backup
-		{
-			putenv("GNUPGHOME=/tmp");
-			$backup->encryptCommand = "echo $params->password | $sPGPname -q -c --batch --no-tty --passphrase-fd 0 $backup->saveTo";
-			$backup->saveTo .= ".gpg";
-			system($backup->encryptCommand);
-			$archiveType = 3;
-		}
+        }
 
-		switch ($params->iArchiveType)
-		{
-			case 0:
-				array_push($backup->headers, "");
-			case 1:
+        if ($params->bEncryptBackup)  #the user has selected an encrypted backup
+        {
+            putenv("GNUPGHOME=/tmp");
+            $backup->encryptCommand = "echo $params->password | $sPGPname -q -c --batch --no-tty --passphrase-fd 0 $backup->saveTo";
+            $backup->saveTo .= ".gpg";
+            system($backup->encryptCommand);
+            $archiveType = 3;
+        }
+
+        switch ($params->iArchiveType)
+        {
+            case 0:
+                array_push($backup->headers, "");
+            case 1:
                 array_push($backup->headers, "Content-type: application/x-zip");
-			case 2:
-				array_push($backup->headers, "Content-type: text/plain");
-			case 3:
-				array_push($backup->headers, "Content-type: application/pgp-encrypted");
-		}
+            case 2:
+                array_push($backup->headers, "Content-type: text/plain");
+            case 3:
+                array_push($backup->headers, "Content-type: application/pgp-encrypted");
+        }
 
-		$backup->filename = substr($backup->saveTo, strrpos($backup->saveTo,"/",-1)+1);
-		array_push($backup->headers, "Content-Disposition: attachment; filename=$backup->filename");
+        $backup->filename = substr($backup->saveTo, strrpos($backup->saveTo,"/",-1)+1);
+        array_push($backup->headers, "Content-Disposition: attachment; filename=$backup->filename");
 
-		return $backup;
-    
-    
+        return $backup;
+
+
     }
-    
-    function download($filename) {
+
+    function download($filename)
+    {
         set_time_limit(0);
         $path = dirname(dirname(__FILE__))."/tmp_attach/ChurchCRMBackups/$filename";
         if (file_exists($path))
@@ -191,22 +193,22 @@ class SystemService {
                     echo $buffer;
                 }
             }
-            fclose ($fd); 
-            exec("rm -rf  ".dirname(dirname(__FILE__))."/tmp_attach/ChurchCRMBackups");
+            fclose ($fd);
+            unlink(dirname(dirname(__FILE__))."/tmp_attach/ChurchCRMBackups");
         }
     }
 
-    function getConfigurationSetting($settingName,$settingValue){
-        
+    function getConfigurationSetting($settingName,$settingValue)
+    {
     }
-    
-    function setConfigurationSetting($settingName,$settingValue){
-        
+
+    function setConfigurationSetting($settingName,$settingValue)
+    {
     }
 
     function getDatabaseVersion()
     {
-         // Check if the table version_ver exists.  If the table does not exist then
+        // Check if the table version_ver exists.  If the table does not exist then
         // SQL scripts must be manually run to get the database up to version 1.2.7
         $bVersionTableExists = FALSE;
         if(mysql_num_rows(RunQuery("SHOW TABLES LIKE 'version_ver'")) == 1) {
@@ -223,15 +225,15 @@ class SystemService {
             return $ver_version;
         }
         return false;
-        
+
     }
-    
+
     function rebuildMenus()
     {
         $root = dirname(dirname(__FILE__));
         $this->playbackSQLtoDatabase($root."/mysql/upgrade/rebuild_nav_menus.sql");
     }
-    
+
     function checkDatabaseVersion()
     {
         $root = dirname(dirname(__FILE__));
@@ -251,7 +253,7 @@ class SystemService {
             $this->playbackSQLtoDatabase($root."/mysql/upgrade/1.2.14-2.0.0.sql");
             return "Upgraded";
         }
-        
+
          // This code will automatically update from 1.3.0 (early build of ChurchCRM)
         if (strncmp($ver_version, "1.3.0", 6) == 0) {
             $old_ver_version = $ver_version;
@@ -264,6 +266,5 @@ class SystemService {
         }
 
         return false;
-        
     }
 }
